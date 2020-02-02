279a496c14215eb3710ba54ac57732eb7be91dc7
Vyacheslav Krasin <interpolatio@gmail.com>
2020-02-03 00:36:40 +0300

    update phonebook

--- phonebook.c
+++ phonebook.c
@@ -6,12 +6,12 @@
 
 // Ниже мы задаём информацию о модуле, которую можно будет увидеть с помощью Modinfo
 MODULE_LICENSE( "GPL" );
-MODULE_AUTHOR( "Alex Petrov <petroff.alex@gmail.com>" );
+MODULE_AUTHOR( "Vyacheslav Krasin" );
 MODULE_DESCRIPTION( "My nice module" );
-MODULE_SUPPORTED_DEVICE( "test" ); /* /dev/testdevice */
+MODULE_SUPPORTED_DEVICE( "phonebook" ); /* /dev/phonebookdevice */
 
 #define SUCCESS 0
-#define DEVICE_NAME "test" /* Имя нашего устройства */
+#define DEVICE_NAME "phonebook" /* Имя нашего устройства */
 
 // Поддерживаемые нашим устройством операции
 static int device_open( struct inode *, struct file * );
@@ -22,7 +22,7 @@ static ssize_t device_write( struct file *, const char *, size_t, loff_t * );
 // Глобальные переменные, объявлены как static, воизбежание конфликтов имен.
 static int major_number; /* Старший номер устройства нашего драйвера */
 static int is_device_open = 0; /* Используется ли девайс ? */
-static char text[ 5 ] = "test\n"; /* Текст, который мы будет отдавать при обращении к нашему устройству */
+static char text[ 10 ] = "phonebook\n"; /* Текст, который мы будет отдавать при обращении к нашему устройству */
 static char* text_ptr = text; /* Указатель на текущую позицию в тексте */
 
 // Прописываем обработчики операций на устройством
@@ -35,9 +35,9 @@ static struct file_operations fops =
  };
 
 // Функция загрузки модуля. Входная точка. Можем считать что это наш main()
-static int __init test_init( void )
+static int __init phonebook_init( void )
 {
- printk( KERN_ALERT "TEST driver loaded!\n" );
+ printk( KERN_ALERT "phonebook driver loaded!\n" );
 
  // Регистрируем устройсво и получаем старший номер устройства
  major_number = register_chrdev( 0, DEVICE_NAME, &fops );
@@ -49,25 +49,25 @@ static int __init test_init( void )
  }
 
  // Сообщаем присвоенный нам старший номер устройства
- printk( "Test module is loaded!\n" );
+ printk( "phonebook module is loaded!\n" );
 
- printk( "Please, create a dev file with 'mknod /dev/test c %d 0'.\n", major_number );
+ printk( "Please, create a dev file with 'mknod /dev/phonebook c %d 0'.\n", major_number );
 
  return SUCCESS;
 }
 
 // Функция выгрузки модуля
-static void __exit test_exit( void )
+static void __exit phonebook_exit( void )
 {
  // Освобождаем устройство
  unregister_chrdev( major_number, DEVICE_NAME );
 
- printk( KERN_ALERT "Test module is unloaded!\n" );
+ printk( KERN_ALERT "phonebook module is unloaded!\n" );
 }
 
 // Указываем наши функции загрузки и выгрузки
-module_init( test_init );
-module_exit( test_exit );
+module_init( phonebook_init );
+module_exit( phonebook_exit );
 
 static int device_open( struct inode *inode, struct file *file )
 {
